2b01d6d253a29c4a6dc09ee37b50f4b29a490c50
9652871015 <115212104+9652871015@users.noreply.github.com>
2022-10-10 11:22:02 +0530

    Default values of global variables
    Default values of global variables

---
+++ A.java
@@ -0,0 +1,14 @@
+class A
+{
+	static int i;
+	static double j;
+	static float k;
+	public static void main(String[] args) 
+	{
+		System.out.println(i);
+		System.out.println(j);
+
+		System.out.println(k);
+		System.out.println(A.i);
+	}
+}
