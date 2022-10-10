0ea639d3287eb87459cf0d0e02513083599db571
9652871015 <115212104+9652871015@users.noreply.github.com>
2022-10-10 10:01:19 +0530

    Static pgm-calling static members in SIB by calling static members directly and along with ClassName.variable
    Static pgm-calling static members in SIB by calling static members directly and along with ClassName.variable

---
+++ Static.java
@@ -0,0 +1,9 @@
+class Static 
+{
+	static int i=45;
+	public static void main(String[] args) 
+	{
+		System.out.println(i);//Calling Static member directly
+		System.out.println(Static.i);//Calling Static member by using Class name .variable
+	}
+}
